abcdc8b1c49ea20c5c93269c72baf6d13d296f17
romgu <gmarca20@gmail.com>
2019-04-11 20:19:54 -0300

    improving script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("I was loaded")
 
+let startButton = document.getElementById("startButton");
+let interButton = document.getElementById("intUnits");
+let engButton = document.getElementById("engUnits");
+
 function transitionWelcome() {
 	let welcome = document.getElementById("welcome").className = "hidden";
 	setTimeout(
@@ -8,6 +12,23 @@ function transitionWelcome() {
 		}, 500);	
 }
 
+function transitionInternational() {
+	let metricMenu = document.getElementById("metricUnits").className = "hidden";
+	setTimeout(
+		function(){ 
+			let intMenu= document.getElementById('intSystem').className = "visible"; 
+		}, 500);	
+}
 
-let startButton = document.getElementById("startButton");
-startButton.addEventListener("click", transitionWelcome)
+function transitionEnglish() {
+	let metricMenu = document.getElementById("metricUnits").className = "hidden";
+	setTimeout(
+		function(){ 
+			let intMenu = document.getElementById('intSystem').className = "visible"; 
+		}, 500);	
+}
+
+
+startButton.addEventListener("click", transitionWelcome);
+interButton.addEventListener("click", transitionInternational);
+engButton.addEventListener("click", transitionEnglish)
